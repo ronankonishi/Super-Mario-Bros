121c1cb53ed317e544b213f64cd285610f977b10
Ronan <rkonishi@ucsd.edu>
2020-12-26 16:09:35 -0800

    implement coins for question blocks

--- src/com/game/object/block/QuestionBlock.java
+++ src/com/game/object/block/QuestionBlock.java
@@ -2,15 +2,19 @@ package com.game.object.block;
 
 import java.awt.Graphics;
 import java.awt.image.BufferedImage;
+import java.util.LinkedList;
 
 import com.game.gfx.Texture;
 import com.game.main.Game;
+import com.game.object.item.Coin;
 
 public class QuestionBlock extends Block{
 	private Texture tex = Game.getTexture();
 	private int index;
 	private BufferedImage[] sprite;
 	private boolean hit;
+	private Coin coin;
+	private boolean disabled;
 	
 	public QuestionBlock(int x, int y, int width, int height, int scale) {
 		super(x, y, width, height, scale);
@@ -20,13 +24,20 @@ public class QuestionBlock extends Block{
 	
 	@Override
 	public void tick() {
-		if (hit) {
+		if (!disabled && hit) {
 			index = 3;
+			coin = new Coin((int) getX(), (int) getY(), (int) getWidth(), (int) getHeight(), (int) getScale());
+			hit = false;
+			disabled = true;
 		}
+		if (coin != null) coin.tick();
+		if (coin != null && coin.shouldRemove()) coin = null;
 	}
 	
 	@Override
-	public void render(Graphics g) {				
+	public void render(Graphics g) {	
+		if (coin != null) coin.render(g);
+		
 		g.drawImage(sprite[index], (int) getX(), (int) getY(), (int) getWidth(), (int) getHeight(), null);
 	}
 	
